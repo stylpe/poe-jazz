2e2edaed4bd887aa1fe984d11dfa3aabe42eed30
Mikal Hitsøy Henriksen <stylpe@gmail.com>
2023-12-30 12:08:30 +0000

    Move -Outfile first and add poe.ninja source for currency

--- Update-Data.ps1
+++ Update-Data.ps1
@@ -1,1 +1,2 @@
-Invoke-RestMethod "https://www.poewiki.net/index.php?title=Special:CargoExport&tables=stackables&&fields=stackables.stack_size%2C+stackables._pageName%2C&where=stackables._pageNamespace+%3D+0&limit=2000&format=json" -OutFile stacks.json
+Invoke-RestMethod -OutFile stacks.json "https://www.poewiki.net/index.php?title=Special:CargoExport&tables=stackables&&fields=stackables.stack_size%2C+stackables._pageName%2C&where=stackables._pageNamespace+%3D+0&limit=2000&format=json"
+Invoke-RestMethod -OutFile currency.json "https://poe.ninja/api/data/currencyoverview?league=Affliction&type=Currency"

a8d98b2d5ea163719de6002119eb5ade8e7afd92
Mikal Hitsøy Henriksen <stylpe@gmail.com>
2023-12-31 01:22:35 +0000

    Add subtyping of currency

--- tiers.ps1
+++ tiers.ps1
@@ -1,6 +1,28 @@
-$data = Get-Content ./currency.json | ConvertFrom-Json | % lines
-$div = $data | where detailsId -eq "divine-orb" | % chaosEquivalent
-$data | select detailsId,chaosEquivalent,@{l="log";e={[math]::Log($_.chaosEquivalent,$div)+1}} | select *,@{l="tier";e={[int] [math]::Max([math]::Floor($_.log*8),0)}} | ft detailsId,tier
+$data = Get-Content ./currency.json | ConvertFrom-Json
+$div = $data.lines | where detailsId -eq "divine-orb" | % chaosEquivalent
+$tiered = $data.lines `
+    | select currencyTypeName, detailsId, chaosEquivalent, @{l="log"; e={[math]::Log($_.chaosEquivalent,$div)}} `
+    | select *,@{l="tier";e={[int] [math]::Max([math]::Floor($_.log*6)+8,0)}}
 
 $stacks=@{}
-Get-Content ./stacks.json | ConvertFrom-Json | % {$stacks[$_._pageName]=$_."stack size"}
+Get-Content ./stacks.json `
+    | ConvertFrom-Json `
+    | % {$stacks[$_._pageName]=$_."stack size"}
+
+$typed=$tiered | select *, `
+    @{l="stack";e={$stacks[$_.currencyTypeName]}}, `
+    @{l="subtype";e={ switch -Regex ($_.currencyTypeName){
+        "Mirror|Hinekora" {"god-tier"}
+        "Tainted" {"scourge";break}
+        "Shard" {"shard";break}
+        "Blessing" {"abyss";break}
+        "Dominance|'s Exalted|of Conflict|Eldritch|Awakener|Veiled" {"influenced";break}
+        "Lifeforce" {"harvest";break}
+        "Sextant|Compass|Scouting" {"atlas";break}
+        "Catalyst" {"catalyst";break}
+        "Oil" {"oil";break}
+        "Ritual" {"other";break}
+        default {"basic"}
+    }}}
+
+$typed | Sort-Object subtype,currencyTypeName | ft currencyTypeName -GroupBy subtype

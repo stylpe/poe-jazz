ad5b8bd60bc25b37e5eb74cdeb6863eb193d6b18
Mikal Hitsøy Henriksen <stylpe@gmail.com>
2024-01-02 22:28:39 +0000

    Synthesize harbinger shard value based on orb result

--- tiers.ps1
+++ tiers.ps1
@@ -8,35 +8,56 @@ $stacks | % { $stacksLookup[$_.name] = $_.size }
 
 function categorize([string]$name) {
     switch -Regex ($name) {
-        "Mirror|Hinekora" { "god-tier"; break }
-        "Key|Valdo" { "key"; break }
-        "Goddess" { "lab"; break }
-        "Tainted" { "scourge"; break }
-        "Shard" { "shard"; break }
-        "Chayula|Uul|Esh|Tul|Xoph" { "abyss"; break }
+        "Mirror|Hinekora" { "GodTier"; break }
+        "Key|Valdo" { "Key"; break }
+        "Goddess" { "Lab"; break }
+        "Tainted" { "Scourge"; break }
+        "Shard" { "Shard"; break }
+        "Chayula|Uul|Esh|Tul|Xoph" { "Breach"; break }
         "Maven's Orb|Dominance|'s Exalted|of Conflict|Eldritch|Awakener|Veiled|Enkindling"
-        { "influenced"; break }
-        "Lifeforce|Sacred Blossom" { "harvest"; break }
-        "Sextant|Compass|Scouting" { "atlas"; break }
+        { "Influenced"; break }
+        "Lifeforce|Sacred Blossom" { "Harvest"; break }
+        "Sextant|Compass|Scouting" { "Atlas"; break }
         "Fragment|Memory|Crescent|Writ|Simulacrum|Timeless|Divine Vessel|Sacrifice|Crest|Mortal|Ritual"
-        { "fragment"; break }
-        "Catalyst" { "catalyst"; break }
-        "Oil" { "oil"; break }
-        "Stacked Deck" { "card"; break }
-        #"Ritual" {"other";break}
-        default { "basic" }
+        { "Fragment"; break }
+        "Catalyst" { "Catalyst"; break }
+        "Oil" { "Oil"; break }
+        "Stacked Deck" { "DivinationCard"; break }
+        default { "Basic" }
     }
 }
 
 $economy = Invoke-RestMethod "https://poe.ninja/api/data/denseoverviews?league=Affliction"
 
 $items = $economy.currencyOverviews + $economy.itemOverviews | % {
-    if ($_.type -eq "Currency") { $type = calc type { categorize $_.name } }
-    else { $ptype = $_.type; $type = calc type { $ptype } }
+    if ($_.type -eq "Currency") { $exp = { categorize $_.name } }
+    else { $ptype = $_.type; $exp = { $ptype } }
+    $type = calc type $exp
     $stackprop = calc stack { $stacksLookup[$_.name] ?? 1 }
     $_.lines | select name, variant, chaos, $type, $stackprop
-}
+} | ? type -NotIn Shard,BaseType,Beast,ClusterJewel,IncursionTemple
+# filtering out some types that are not yet handled properly
+# or that get replaced (i.e. shards)
 
+# Synthesized shard prices
+$basic = $items | ? type -In Basic,GodTier
+$orbToShard = $stacks `
+| ? name -match " Shard$" `
+| % {
+        $chaos = if($_.name -eq "Chaos Shard") {1} else {
+            $commonPart = $_.name -replace " Shard",""
+            $basic | ? name -match $commonPart | % chaos | select -First 1
+        }
+        [PSCustomObject]@{
+            name = $_.name
+            chaos = $chaos / $_.size
+            type = "Shard"
+            stack = $_.size
+        }
+    }
+$items += $orbToShard
+
+# Calculate tiers
 $div = $items | where name -eq "Divine Orb" | % chaos
 $tierprop = calc tier {
     [int] [math]::Max(

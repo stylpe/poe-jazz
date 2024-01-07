c5b23fd0edb9431a73a07dd3067535d07a86978c
Mikal Hitsøy Henriksen <stylpe@gmail.com>
2024-01-07 18:08:11 +0000

    Generate stacks, plus refactors

--- tiers.ps1
+++ tiers.ps1
@@ -2,11 +2,9 @@ function calc([string]$name, $e) {
     return @{ name = $name; e = $e }
 }
 
-$stacks = Invoke-RestMethod "https://www.poewiki.net/index.php?title=Special:CargoExport&tables=stackables&&fields=stackables.stack_size%3Dsize%2C+stackables._pageName%3Dname%2C&where=stackables._pageNamespace+%3D+0&limit=2000&format=json"
-$stacksLookup = @{}
-$stacks | % { $stacksLookup[$_.name] = $_.size }
 $economy = Invoke-RestMethod "https://poe.ninja/api/data/denseoverviews?league=Affliction"
 
+# Currencies and Fragments get some custom variants
 function categorize([string]$name) {
     switch -Regex ($name) {
         "Mirror|Hinekora" { "GodTier"; break }
@@ -15,12 +13,14 @@ function categorize([string]$name) {
         "Tainted" { "Scourge"; break }
         "Shard" { "Shard"; break }
         "Chayula|Uul|Esh|Tul|Xoph" { "Breach"; break }
-        "Maven's Orb|Dominance|'s Exalted|of Conflict|Eldritch|Awakener|Veiled|Enkindling"
-        { "Influenced"; break }
+        "Maven's Orb|Dominance|'s Exalted|of Conflict|Eldritch|Awakener|Veiled|Enkindling" { "Influenced"; break }
         "Lifeforce|Sacred Blossom" { "Harvest"; break }
-        "Sextant|Compass|Scouting" { "Atlas"; break }
-        "Fragment|Memory|Crescent|Writ|Simulacrum|Timeless|Divine Vessel|Sacrifice|Crest|Mortal|Ritual"
-        { "Fragment"; break }
+        "Sextant|Compass|Scouting|Divine Vessel" { "Atlas"; break }
+        "Memory" { "Memory"; break }
+        "Crest" { "Elderslayer"; break }
+        "Ritual" { "Ritual"; break }
+        "Timeless" { "Legion"; break }
+        "Fragment|Crescent|Writ|Simulacrum|Sacrifice|Mortal" { "Boss"; break }
         "Catalyst" { "Catalyst"; break }
         "Oil" { "Oil"; break }
         "Stacked Deck" { "DivinationCard"; break }
@@ -28,51 +28,84 @@ function categorize([string]$name) {
     }
 }
 
-# filtering out some types that are not yet handled properly
-# or that get replaced (i.e. shards), then expanding some properties
-$items = $economy.currencyOverviews + $economy.itemOverviews `
-| ? type -NotIn Shard,BaseType,Beast,ClusterJewel,IncursionTemple,HelmetEnchant `
-| % {
-    if ($_.type -eq "Currency") { $exp = { categorize $_.name } }
-    else { $ptype = $_.type; $exp = { $ptype } }
-    $type = calc type $exp
-    $stackprop = calc stack { $stacksLookup[$_.name] ?? 1 }
-    $variantprop = calc variant { $_.variant -split ", " }
-    $_.lines | select name, $variantprop, chaos, $type, $stackprop
-}
+$currency = $economy | % currencyOverviews -PipelineVariable group | % lines |
+    select name, chaos, (calc type {$group.type}), (calc variant { categorize $_.name }) |
+    ? variant -ne "Shard" # these get calculated
+$items = $currency
 
-# Get a summary of variants:
-# $items | group type | fl Name,@{n="Variants";e={$_.group.variant | Sort-Object -Unique | Join-String -Separator ", "}}
+# filtering out some types that are not yet handled properly
+$skippedTypes = "BaseType|Beast|ClusterJewel|IncursionTemple|HelmetEnchant|Unique|SkillGem"
+$items += $economy.itemOverviews |
+    ? type -NotMatch $skippedTypes -PipelineVariable group |
+    % lines |
+    select name, chaos, (calc type {$group.type}), (calc variant { $_.variant -split ", " })
 
 # Synthesized shard prices
-$basic = $items | ? type -In Basic,GodTier
+$basic = $currency | ? variant -In Basic,GodTier
 $orbToShard = $stacks `
 | ? name -match " Shard$" `
 | % {
         $chaos = if($_.name -eq "Chaos Shard") {1} else {
             $commonPart = $_.name -replace " Shard",""
-            $basic | ? name -match $commonPart | % chaos | select -First 1
+            $basic | ? name -match $commonPart | select -First 1 -expand chaos
         }
         [PSCustomObject]@{
             name = $_.name
             chaos = $chaos / $_.size
-            type = "Shard"
-            stack = $_.size
+            type = "Currency"
+            variant = "Shard"
         }
     }
 $items += $orbToShard
+
+# Get a summary of variants:
+# $items | group type | fl Name,@{n="Variants";e={$_.group.variant | Sort-Object -Unique | Join-String -Separator ", "}}
 
 # Calculate map tiers
 $maptierprop = calc MapTier { @($_.variant) -like "T*" -replace "T","" -as [int[]] }
 $maps = $items | ? type -Like "*Map" | select *,$maptierprop
 
 # Calculate tiers
+# My note progression is C D# E F# G Bb
+# I want Chaos Orb and Divine orb to be anchors at C4 and C5.
 $div = $items | where name -eq "Divine Orb" | % chaos
-$tierprop = calc tier {
-    [int] [math]::Max(
-        [math]::Floor(
-            [math]::Log($_.chaos, $div) * 6
-        ) + 8, 0
-    )
+# I want the lowest note to be Bb2.
+# I'll define tiers as higher is better.
+# So Bb2 is T1, C3 is T2, C4 is T8 (chaos), C5 is T14 (div), C6 is T20.
+# I'll use an exponential scale to divide tiers.
+# x^0=1 (chaos) at T8 and x^6 = div at T14
+$base=[math]::Pow($div, 1/6) # sixth root
+$tierBreakpoints = 1..20 | % { [pscustomobject]@{
+    Tier=$_
+    Chaos=($_ -eq 1 ? 0 : [math]::pow($base,$_-8))
+} }
+function toTier($value) {
+    @($tierBreakpoints | ? chaos -LE $value)[-1].Tier
 }
+
+$tierprop = calc tier { toTier $_.chaos }
 $tiered = $items | select *, $tierprop
+# Test the math:
+# $tiered | group tier | select name, {$_.group | measure chaos -Minimum -Maximum | select * } | select name -ExpandProperty "$*" | select Name,@{n="BreakPoint";e={$tierBreakpoints | where t -eq $_.Name | % c} },Minimum,Maximum | ft *,{$_.Minimum -ge $_.BreakPoint}
+
+# Generate item stacks for each tier
+$stacks = Invoke-RestMethod "https://www.poewiki.net/index.php?title=Special:CargoExport&tables=stackables&&fields=stackables.stack_size%3Dsize%2C+stackables._pageName%3Dname%2C&where=stackables._pageNamespace+%3D+0&limit=2000&format=json"
+$stacksLookup = @{}
+$stacks | ? size -gt 1 | % { $stacksLookup[$_.name] = $_.size }
+$stackable = $tiered | ? name -in $stacksLookup.Keys
+$stacked = $stackable | % {
+    $item = $_
+    $fullStack=$stacksLookup[$item.name]
+    $fullStackValue = $item.chaos * $fullStack
+    $maxTier = toTier $fullStackValue
+    if ($maxTier -eq $item.tier) { return }
+    $tierBreakpoints | ? tier -in (($item.tier+1)..$maxTier) | % {
+        $tierStack = [int][math]::Ceiling($_.chaos/$item.chaos)
+        $tier = $_.tier
+        $item | select -ExcludeProperty chaos,tier *,
+            (calc chaos {$_.chaos*$tierStack}),
+            (calc tier {$tier}),
+            (calc stack {$tierStack})
+    }
+}
+$tiered += $stacked
